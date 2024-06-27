5cb168edc38685fe78291cbdcfcf7eb3d10e070f
Oelmaier, Mark <oelmaier.mark@gmail.com>
2024-06-27 10:01:14 +0200

    Praktikum 1 - base files

---
+++ Praktikum_1/and2.vhdl
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity AND2 is
+  port (x, y: in std_logic; z: out std_logic);
+end AND2;
+
+
+architecture DATAFLOW of AND2 is
+begin
+  z <= x and y;
+end architecture;
